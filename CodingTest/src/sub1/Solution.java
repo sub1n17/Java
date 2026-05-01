1c799738232df35639a7f174e85f689331fecc34
sub1n17 <sub1n17@naver.com>
2026-05-01 15:08:33 +0900

    add CodingTest

---
+++ CodingTest/src/sub1/Solution.java
@@ -0,0 +1,17 @@
+package sub1;
+
+/* 날짜 : 2026/05/01
+ * 이름 : 최수빈
+ * 내용 : 프로그래머스 기출문제 1번 문자출력
+ */
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
