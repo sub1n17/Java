098abae962d82c65eeda8dc34a8668d2fe08d548
sub1n17 <sub1n17@naver.com>
2026-05-01 15:09:40 +0900

    add Textbook

---
+++ Textbook/src/ch02/sec06/TextBlockExample.java
@@ -0,0 +1,23 @@
+package ch02.sec06;
+
+public class TextBlockExample {
+	public static void main(String[] args) {
+		//p.54
+		String str1 = "" + "{\n" + "\t\"id\":winter\",\n" + "\t\"name\":눈송이\"\n" + "}";
+		String str2 = """
+				{
+					"id" : "winter",
+					"name": "눈송이"
+				}
+				""";
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		String str = """
+			나는 자바를 \
+			학습합니다.
+			나는 자바 고수가 될 겁니다
+			""";
+		System.out.println(str);
+	}
+}
