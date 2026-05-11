ee920fa211f66ba21e9d30fd1d1584fd46fe8461
sub1n17 <sub1n17@naver.com>
2026-05-11 12:44:01 +0900

    add Textbook

---
+++ Textbook/src/ch05/sec12/Week.java
@@ -0,0 +1,11 @@
+package ch05.sec12; //p197	
+
+public enum Week {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
---
+++ Textbook/src/ch06/sec06/exam01/Car.java
@@ -0,0 +1,8 @@
+package ch06.sec06.exam01; // p. 217
+
+public class Car {
+	// 필드 선언
+	String model;
+	boolean start;
+	int speed;
+}
